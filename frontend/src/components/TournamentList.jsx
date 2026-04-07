e7ed4fbcbf0224aad180ecf5ba2467e3bd4fc4bd
huynd140196 <huyndde140196@fpt.edu.vn>
2026-04-07 22:40:05 +0700

    test tailwind bug 2.0

--- frontend/src/components/TournamentList.jsx
+++ frontend/src/components/TournamentList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Users, Calendar } from "lucide-react";
+import { Users, Calendar } from "@material-tailwind/react";
 
 const TournamentList = () => {
   const [tournaments, setTournaments] = useState([]);
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // Đảm bảo đã có Tailwind CSS
-
-// THÊM DÒNG NÀY
-import { ThemeProvider } from "@material-tailwind/react";
+import "./index.css";
+import { ThemeProvider } from "@material-tailwind/react"; // Kiểm tra dòng này
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* BAO BỌC APP TẠI ĐÂY */}
     <ThemeProvider>
       <App />
     </ThemeProvider>

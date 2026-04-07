194c1c944e727f86602f610bdddec1b0675b2c09
huynd140196 <huyndde140196@fpt.edu.vn>
2026-04-07 15:26:40 +0700

    Update CORS

--- core_config/settings.py
+++ core_config/settings.py
@@ -56,8 +56,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "https://tournament-pro-client.onrender.com",
 ]
 
 ROOT_URLCONF = 'core_config.urls'

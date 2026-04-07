9dc58aecafcae616f45af5cb649369d424b9fd37
huynd140196 <huyndde140196@fpt.edu.vn>
2026-04-07 15:52:06 +0700

    login logic

--- core_config/urls.py
+++ core_config/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tournaments/', include('tournaments.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
